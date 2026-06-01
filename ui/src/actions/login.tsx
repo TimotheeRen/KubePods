d294053fa2440a3579a98dd9e07f4acd491dbd33
= <=>
2026-06-01 19:39:20 +0200

    feat(ui): add env detection

--- ui/src/actions/login.tsx
+++ ui/src/actions/login.tsx
@@ -7,7 +7,8 @@ const User = z.object({
 })
 
 export async function login({ request }: ActionFunctionArgs) {
-  const host = "http://localhost:3001"
+  const host = import.meta.env.VITE_API_HOST;
+  // const host = "http://localhost:3001"
   console.log(host)
   const formData = await request.formData()
   const result = User.safeParse(Object.fromEntries(formData.entries()))
