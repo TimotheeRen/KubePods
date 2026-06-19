f26207b331e6270e5776282eb553cf83a8549d0e
TimotheeRen <timothee.ren@proton.me>
2026-06-19 11:22:00 +0200

    init(doc): add documentation

---
+++ doc/src/lib/shared.ts
@@ -0,0 +1,10 @@
+export const appName = 'KubePods';
+export const docsRoute = '/docs';
+export const docsImageRoute = '/og/docs';
+export const docsContentRoute = '/llms.mdx/docs';
+
+export const gitConfig = {
+  user: 'TimotheeRen',
+  repo: 'KubePods',
+  branch: 'main',
+};
