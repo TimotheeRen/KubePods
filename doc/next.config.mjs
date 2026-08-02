0bb079193b90b996a5afeca0d5a3b4812a2abdba
TimotheeRen <timothee.ren@proton.me>
2026-08-02 15:59:35 +0000

    chore: migrate dependencies to devpod environment

--- doc/next.config.mjs
+++ doc/next.config.mjs
@@ -5,6 +5,9 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  allowedDevOrigins: [
+    '172.17.0.3',
+  ],
 };
 
 export default withMDX(config);
